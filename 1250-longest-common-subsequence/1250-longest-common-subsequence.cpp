c92187f3eec9d40ef801c7425cb1e782cbb46775
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-15 11:41:02 +0530

    Time: 22 ms (79.94%), Space: 27.4 MB (50.28%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -22,25 +22,22 @@ int solve(int index1, int index2, string& text1, string& text2, vector<vector<in
 
     return dp[index1][index2] = (0 + max(solve(index1 - 1, index2, text1, text2,dp), solve(index1, index2 - 1, text1,text2,dp))); 
 }
-    int longestCommonSubsequence(string text1, string text2) {
+     int longestCommonSubsequence(string text1, string text2) {
         
         int n = text1.length();
         int m = text2.length();
 
-        // vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
 
         // return solve(n, m,text1, text2, dp);
 
-        // for(int i = 0; i<n; i++)
-        // {
-        //     dp[i][0] = 0;
-        // }
-
-        vector<int> prev(m+1, 0), curr(m+1,0);
-
+        for(int i = 0; i<n; i++)
+        {
+            dp[i][0] = 0;
+        }
         for(int j = 0; j<m; j++)
         {
-            prev[j] = 0;
+            dp[0][j] = 0;
         }
         
 
@@ -50,14 +47,44 @@ int solve(int index1, int index2, string& text1, string& text2, vector<vector<in
             {
                 if(text1[i-1] == text2[j-1])
                 {
-                    curr[j] = 1 + prev[j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
 
-                else curr[j] = max(prev[j], curr[j-1]);
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
-            prev = curr;
+        }
+        
+        int len =  dp[n][m];
+        string ans = "";
+        for(int i = 0; i<len; i++)
+        {
+            ans += '$';
         }
 
-        return prev[m];
+        int i = n;
+        int j = m;
+
+        int index = len-1;
+        while(i > 0 && j > 0)
+        {
+            if(text1[i-1] == text2[j-1])
+            {
+                ans[index] += text1[i-1];// assign krdo ans me value
+                index--;
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        cout << ans << endl;
+        return dp[n][m];
     }
 };
