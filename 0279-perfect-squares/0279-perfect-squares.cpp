1fc8b4d10c2f276f8b95959dffdb9ff5bc1b26d7
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-15 14:21:14 +0530

    Time: 41 ms (74.37%), Space: 13.1 MB (66.85%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
+    int solve(int n, vector<int>& dp) {
+        if (n == 0) {
+            return 0;
+        }
 
-int solve(int n, vector<int>& dp)
-{
-    if(n == 0)
-    {
-        return 0;
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int mincount = INT_MAX;
+        for (int i = 1; i * i <= n; i++) {
+            int result = 1 + solve(n - (i * i), dp);
+            mincount = min(mincount, result);
+        }
+
+        return dp[n] = mincount;
     }
-
-    if(dp[n] != -1)
-    {
-        return dp[n];
-    }
-
-    int mincount = INT_MAX;
-    for(int i = 1; i*i <= n; i++)
-    {
-        int result = 1 + solve(n-(i*i), dp);
-        mincount = min(mincount, result);
-    }
-
-    return dp[n] = mincount;
-}
     int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0; // base case
 
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
-        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+
+        return dp[n];
     }
 };
