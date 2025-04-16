d5d8298664aa961c4d0ac939951adf703d5cf31b
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-16 15:05:04 +0530

    Time: 87 ms (42.48%), Space: 76 MB (21.15%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -12,22 +12,23 @@ int lcs(string& s, string& t)
     {
         dp[i][0] = 0;
     }
-
-    for(int j = 0; j <= m;j++)
+    for(int j = 0; j<=m; j++)
     {
         dp[0][j] = 0;
     }
+
+    // we have created the dp table for base case
     for(int i = 1; i<=n; i++)
     {
-        for(int j = 1; j<=m; j++)
+        for(int j = 1;j<=m; j++)
         {
             if(s[i-1] == t[j-1])
             {
-                dp[i][j] = 1+ dp[i-1][j-1];
+                dp[i][j] = 1 + dp[i-1][j-1];
             }
             else
             {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] =  max(dp[i-1][j], dp[i][j-1]);
             }
         }
     }
