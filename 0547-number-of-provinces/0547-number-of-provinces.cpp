d238834194b57020ba1f11603e4be50a9fd4108d
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-17 09:46:54 +0530

    Time: 7 ms (17.08%), Space: 21 MB (16.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
 
-void dfs(int node, stack<int>& st, vector<int>& visited,vector<vector<int>>& adj)
+void dfs(int node, stack<int>& st, vector<bool>& visited,vector<vector<int>>& adj)
 {
-    visited[node] = 1;
+    visited[node] = true;
     st.push(node);
 
-    for(auto adjnode: adj[node])
+    for(auto adjnode:adj[node])
     {
         if(!visited[adjnode])
         {
-            dfs(adjnode, st, visited, adj);
+            dfs(adjnode,st,visited,adj);
         }
     }
 }
     int findCircleNum(vector<vector<int>>& isConnected) {
 
-        int n =  isConnected.size();
+        int n = isConnected.size();
         vector<vector<int>> adj(n+1);
-
         for(int i = 0; i<n; i++)
         {
             for(int j = 0; j<n; j++)
@@ -30,21 +29,22 @@ void dfs(int node, stack<int>& st, vector<int>& visited,vector<vector<int>>& adj
                 }
             }
         }
-
-        // wehave created the adjacency list
-        vector<int> visited(n,0);
+        // we have constructed our adjacency list first
+        int provinces = 0;
         stack<int> st;
-        int count = 0;
-
-        for(int i = 0;i < n; i++)
+        vector<bool> visited(n+1,false);
+        for(int i = 0; i<n; i++)
         {
             if(!visited[i])
             {
-                count++;
-                dfs(i,st,visited,adj);
+                provinces++;
+                dfs(i, st, visited,adj);
+
             }
         }
 
-        return count;        
+        return provinces;
+
+        
     }
 };
