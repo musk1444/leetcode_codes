5331e68394e82f4f3e76214739aca2542b83bab6
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-17 10:39:13 +0530

    Time: 31 ms (57.42%), Space: 44.2 MB (39.92%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -29,9 +29,36 @@ int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp)
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1,0));
 
-        return solve(n,m,s , t,dp);
+        // return solve(n,m,s , t,dp);
+        for(int i = 0; i<=n; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j<=m; j++)
+        {
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                if(s[i-1] == t[j-1])
+                {
+                    if(dp[i-1][j] <= 1e10 && dp[i-1][j-1] <= 1e10)
+                    {
+                        dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    }
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
         
+        return dp[n][m];
     }
 };
