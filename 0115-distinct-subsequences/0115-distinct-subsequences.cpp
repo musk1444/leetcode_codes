22ac40297f517374724b902a1b4cd947ad26abf8
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-17 10:26:57 +0530

    Time: 35 ms (34.78%), Space: 27.1 MB (46.63%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,18 +3,15 @@ public:
 
 int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp)
 {
-    // base case
-    if(j==0)
+    if(j == 0)
     {
-        // means we have found a match for all of the tsr2
+        // j exhaust hogya matlb ab matching hogyi poori str2 ki
         return 1;
     }
     if(i == 0)
     {
         return 0;
-        // we have exhausted our str1 but mila nai match
     }
-
     if(dp[i][j] != -1)
     {
         return dp[i][j];
@@ -22,46 +19,19 @@ int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp)
 
     if(s[i-1] == t[j-1])
     {
-        // match milggya hume
-        return (solve(i-1,j-1,s,t,dp) + solve(i-1,j,s,t,dp));
+        return dp[i][j] = solve(i-1,j-1,s,t,dp) + solve(i-1,j,s,t,dp);
     }
-    
+
     return dp[i][j] = solve(i-1,j,s,t,dp);
-   
 }
     int numDistinct(string s, string t) {
 
         int n = s.length();
         int m = t.length();
 
-        vector<vector<long long>> dp(n+1, vector<long long>(m+1,0));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
 
-        // return solve(n, m, s, t,dp);
-        for(int i = 0; i<=n; i++)
-        {
-            dp[i][0] = 1;
-        }
-        for(int j = 1; j<=m; j++)
-        {
-            dp[0][j] = 0;
-        }
-        // we will loop through normally
-        for(int i = 1; i<=n; i++)
-        {
-            for(int j = 1; j<=m; j++)
-            {
-                if(s[i-1] == t[j-1])
-                {
-                    if(dp[i-1][j-1] <= 1e10 && dp[i-1][j] <= 1e10)
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
+        return solve(n,m,s , t,dp);
         
-        return (int) dp[n][m];
     }
 };
