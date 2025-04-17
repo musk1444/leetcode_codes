418be786aa917f168c0a0e49d41bee2942b37bf0
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-17 12:41:43 +0530

    Time: 7 ms (60.69%), Space: 13.4 MB (23.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -34,9 +34,38 @@ int solve(int i, int j, string& s, string& t,vector<vector<int>>& dp)
         int n = word1.length();
         int m = word2.length();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
 
-        return solve(n,m,word1,word2,dp);
+
+        // return solve(n,m,word1,word2,dp);
+        for(int i = 0; i<=n; i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int j = 0; j<=m; j++)
+        {
+            dp[0][j] = j;
+        }
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    int insertion = 1 + dp[i][j-1];
+                    int deletion = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({insertion, deletion, replace});
+                }
+            }
+        }
+        return dp[n][m];
         
     }
 };
