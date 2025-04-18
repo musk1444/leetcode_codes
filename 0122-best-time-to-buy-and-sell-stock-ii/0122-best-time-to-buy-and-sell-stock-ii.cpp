5452402e0ae810224708d6af124022a1c507da1b
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-18 17:45:17 +0530

    Time: 0 ms (100%), Space: 17.1 MB (84.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,68 +1,22 @@
 class Solution {
 public:
-    int n;
-    int solve(int index, int buy, vector<int>& prices,
-              vector<vector<int>>& dp) {
-        if (index == n) {
-            return 0;
-        }
-        if (dp[index][buy] != -1) {
-            return dp[index][buy];
-        }
-
-        int profit = INT_MIN;
-
-        if (buy == 1) {
-            // we are allowed to purchase
-            int take = -prices[index] + solve(index + 1, 0, prices, dp);
-            int not_take = 0 + solve(index + 1, 1, prices, dp);
-            profit = max(take, not_take);
-        } else {
-            // agar buy nhi kr skte
-            // means we have to sell
-            int take = prices[index] + solve(index + 1, 1, prices, dp);
-            int not_take = 0 + solve(index + 1, 0, prices, dp);
-            profit = max(take, not_take);
-        }
-
-        return dp[index][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
 
-        n = prices.size();
+        int n = prices.size();
+        int aheadnotbuy = 0;
+        int aheadbuy = 0;
+        
+        int currnotbuy, currbuy;
+        for(int i = n-1; i>= 0; i--)
+        {
+            currnotbuy = max((prices[i] + aheadbuy), (0 + aheadnotbuy));
 
-        // vector<vector<int>> dp(n+1, vector<int>(2,-1));
-        // vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+            currbuy = max( (-prices[i] + aheadnotbuy), (0 + aheadbuy));
 
-        // return solve(0,1,prices,dp);
-
-        vector<int> ahead(2,0), curr(2,0);
-        for (int i = 0; i < 2; i++) {
-            ahead[i] = 0;
+            aheadbuy = currbuy;
+            aheadnotbuy = currnotbuy;
         }
 
-        for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 0; buy < 2; buy++) {
-                int profit = INT_MIN;
-
-                if (buy == 1) {
-                    // we are allowed to purchase
-                    int take = -prices[index] + ahead[0];
-                    int not_take = 0 + ahead[1];
-                    profit = max(take, not_take);
-                } else {
-                    // agar buy nhi kr skte
-                    // means we have to sell
-                    int take = prices[index] + ahead[1];
-                    int not_take = 0 + ahead[0];
-                    profit = max(take, not_take);
-                }
-
-                curr[buy] = profit;
-            }
-            ahead = curr;
-        }
-        return ahead[1];
+        return aheadbuy;
     }
-
 };
