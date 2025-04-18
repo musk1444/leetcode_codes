be38876e4344d0e673cc63c7db2fdd95e7f87ef2
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-18 20:14:31 +0530

    Time: 52 ms (80.23%), Space: 21.2 MB (47.43%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,44 +2,43 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        // set<string> myset(wordList.begin(), wordList.end());
-        unordered_set<string> myset(wordList.begin(), wordList.end());
-        if(myset.find(endWord) == myset.end()) return 0;
-        if(beginWord == endWord)
-        {
-            return 0;
-        }
+        unordered_set<string> wordset(wordList.begin(), wordList.end());
+        if(beginWord == endWord) return 0;
 
         queue<pair<string,int>> q;
-        q.push({beginWord, 1});
-        myset.erase(beginWord);
+        q.push({beginWord,1});
+        wordset.erase(beginWord);
+
         while(!q.empty())
         {
-            string word = q.front().first;
-            int steps = q.front().second;
+            // jab tak queue khali nai hojata
+            auto it = q.front();
             q.pop();
 
-            if(word == endWord) return steps;
+            string word = it.first;
+            int steps = it.second;
 
+            if(word == endWord)
+            {
+                return steps;
+            }
             for(int i = 0; i<word.length(); i++)
             {
                 char originalchar = word[i];
-                for(char ch = 'a';  ch <= 'z'; ch++)
+                for(char ch = 'a'; ch<='z';ch++)
                 {
                     word[i] = ch;
-                    if(myset.count(word))
+                    if(wordset.find(word) != wordset.end())
                     {
-                        myset.erase(word);
-                        q.push({word, steps+1});
+                        // we have found the word in our set
+                        wordset.erase(word);
+                        q.push({word,steps+1});
                     }
                 }
+
                 word[i] = originalchar;
-
             }
-
         }
-
         return 0;
-        
     }
 };
