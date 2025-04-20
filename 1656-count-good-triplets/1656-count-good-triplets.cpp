3146cb42b74f0341d94237ea35661c350a0da0e4
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-20 15:41:35 +0530

    Time: 6 ms (72.72%), Space: 11 MB (74.83%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -5,9 +5,9 @@ public:
         int n = arr.size();
         int count = 0;
 
-        for(int i = 0; i<= n-1; i++)
+        for(int i = 0; i<= n-3; i++)
         {
-            for(int j = i+1; j<= n-1; j++)
+            for(int j = i+1; j<= n-2; j++)
             {
                 if(abs(arr[i] - arr[j]) <= a)
                 {
