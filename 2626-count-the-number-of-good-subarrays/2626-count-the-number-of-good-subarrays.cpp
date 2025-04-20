e341ec9f50fb90e9b7dcf0f22493800dd727b17a
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-20 16:21:41 +0530

    Time: 71 ms (86.52%), Space: 79.1 MB (47.7%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-
-        int n = nums.size();
         
-        unordered_map<int,int> mpp;
-        long long pairs = 0;
-        long long count = 0;
-
+        int n = nums.size();
         int i = 0;
         int j = 0;
+        long long pairs = 0;
+        long long result = 0;
+
+        unordered_map<int,int> mpp;
         while(j < n)
         {
-            pairs += mpp[nums[j]]; // we keep adding the number of time swe have prevoiosuly seen nums[j];
+            pairs += mpp[nums[j]];
             mpp[nums[j]]++;
 
             while(pairs >= k)
             {
-                // jab tak hume ans mil rh ah humara
-                count += (n-j);
-                // we need to shrink the window as well
                 mpp[nums[i]]--;
+                // decreease the frequency
+                
                 pairs -= mpp[nums[i]];
+                // decrease it from pairs as well
+                result += (n-j);
                 i++;
             }
-
             j++;
+
         }
-
-        return count;
-
-        
+        return result;
     }
 };
