994514d8fbf1c1cda288574677ebee2f7a490932
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-20 14:16:45 +0530

    Time: 56 ms (55.22%), Space: 60.5 MB (38.5%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,14 +1,26 @@
 class Solution {
-    public:
-        long long countFairPairs(vector<int>& nums, int lower, int upper) {
-            long long ans = 0;
-            sort(nums.begin(), nums.end());
-            for (int i = 0; i + 1 < nums.size(); i++) {
-                int minReq = lower - nums[i], maxReq = upper - nums[i];
-                auto high = upper_bound(nums.begin() + i + 1, nums.end(), maxReq);
-                auto low = lower_bound(nums.begin() + i + 1, nums.end(), minReq);
-                ans += (high - low);
-            }
-            return ans;
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+
+        int n = nums.size();
+
+        sort(begin(nums), end(nums));
+        long long result = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            int index1 = lower_bound(nums.begin() + i + 1, nums.end(), lower-nums[i]) - nums.begin();
+            int x = index1 - 1 - i;
+            // the first eement NOT LESS THAN lower - nums[i];
+
+            int index2 = upper_bound(nums.begin() + i + 1, nums.end(), upper-nums[i]) - nums.begin();
+            int y = index2 - 1 - i;
+            // the first eement GREATER THAN upper - nums[i];
+
+
+            result += (y-x);
         }
-    };
+        
+        return result;
+    }
+};
