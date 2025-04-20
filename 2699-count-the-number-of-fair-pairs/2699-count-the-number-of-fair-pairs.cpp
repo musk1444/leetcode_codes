069e14318a02aeed57f814698e6abddb6eea5ab0
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-20 14:41:03 +0530

    Time: 66 ms (18.29%), Space: 60.5 MB (38.5%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+int lowerbound(int low, int high, int target, vector<int>& nums)
+{
+    int result = -1;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        if(nums[mid] >= target)
+        {
+            result = mid;
+            high = mid-1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    return result == -1 ? high + 1 : result; // return insertion point
+
+}
+
+int upperbound(int low, int high, int target, vector<int>& nums)
+{
+    int result = -1;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        if(nums[mid] > target)
+        {
+            result = mid;
+            high = mid-1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    return result == -1 ? high + 1 : result; // return insertion point
+    
+}
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
 
         int n = nums.size();
@@ -9,16 +48,18 @@ public:
 
         for(int i = 0; i<n; i++)
         {
-            int index1 = lower_bound(nums.begin() + i + 1, nums.end(), lower-nums[i]) - nums.begin();
-            int x = index1 - 1 - i;
             // the first eement NOT LESS THAN lower - nums[i];
+            // int index1 = lower_bound(nums.begin() + i + 1, nums.end(), lower-nums[i]) - nums.begin();
+            int index1 = lowerbound(i+1, n-1,lower-nums[i], nums);
 
-            int index2 = upper_bound(nums.begin() + i + 1, nums.end(), upper-nums[i]) - nums.begin();
-            int y = index2 - 1 - i;
             // the first eement GREATER THAN upper - nums[i];
-
-
-            result += (y-x);
+            // int index2 = upper_bound(nums.begin() + i + 1, nums.end(), upper-nums[i]) - nums.begin();
+            int index2 = upperbound(i+1, n-1,upper-nums[i], nums);
+            
+            if(index1 != -1 && index2 != -1)
+            {
+                result += (index2 - index1);
+            }
         }
         
         return result;
