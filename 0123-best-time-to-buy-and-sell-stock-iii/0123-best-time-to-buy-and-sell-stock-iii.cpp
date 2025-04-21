6d847613ef67ea655445b7f115544003707b7d37
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-21 15:19:29 +0530

    Time: 453 ms (19.76%), Space: 223.8 MB (28.22%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int n;
 
-int solve(int index, int buy, int cap,vector<int>& prices, vector<vector<vector<int>>>& dp)
+int solve(int index, int buy, int cap, vector<int>& prices,vector<vector<vector<int>>>& dp)
 {
     if(cap == 0)
     {
@@ -12,33 +12,37 @@ int solve(int index, int buy, int cap,vector<int>& prices, vector<vector<vector<
     {
         return 0;
     }
-
     if(dp[index][buy][cap] != -1)
     {
         return dp[index][buy][cap];
     }
-    int profit = INT_MIN;
+    int profit = INT_MAX;
 
-    if(buy)
+    if(buy == 1)
     {
-        profit = max ((-prices[index] + solve(index+1, 0, cap, prices,dp)),
-        ( 0 + solve(index+1,1,cap, prices,dp)));
+        int take = -prices[index] + solve(index+1, 0,cap,prices,dp);
+        int nottake = 0 + solve(index+1,1, cap, prices,dp);
+        profit = max(take,nottake);
     }
-    else
+
+    else // sell krne ka option h
     {
-        profit = max((prices[index] + solve(index+1, 1, cap-1, prices,dp)),
-        (0 + solve(index+1,0,cap, prices,dp)));
+        int take = +prices[index] + solve(index+1, 1,cap-1,prices,dp);
+        int nottake = 0 + solve(index+1,0, cap, prices,dp);
+        profit = max(take,nottake);
     }
 
     return dp[index][buy][cap] = profit;
 }
+
     int maxProfit(vector<int>& prices) {
+
         n = prices.size();
 
         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
 
         return solve(0,1,2,prices,dp);
-        // index, buy, cap
+        // {index, buy, cap}
         
     }
 };
