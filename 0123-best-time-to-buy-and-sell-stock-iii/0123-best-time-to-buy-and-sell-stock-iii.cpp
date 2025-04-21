8ab3fec4d1ba87ccc93acb11deb5f97cbd898354
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-21 16:30:46 +0530

    Time: 160 ms (53.4%), Space: 127.6 MB (55.87%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -34,9 +34,41 @@ int solve(int index, int tr, vector<int>& prices, vector<vector<int>>& dp)
 
         n = prices.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(4,-1));
-        return solve(0,0, prices,dp);
+        // vector<vector<int>> dp(n+1, vector<int>(4,-1));
+        vector<vector<int>> dp(n+1, vector<int>(4,0));
+
+        // return solve(0,0, prices,dp);
         // index, transaction
-         
+
+        for(int tr =0; tr<4; tr++)
+        {
+            dp[n][tr] = 0;
+        }
+       
+
+        for(int index = n-1; index >=0; index--)
+        {
+            for(int tr = 0; tr<4; tr++)
+            {
+                int profit = INT_MIN;
+
+                if(tr % 2 == 0) // even index means we can buy
+                {
+                    int take = -prices[index] + solve(index+1, tr+1,prices,dp);
+                    int nottake = 0 + solve(index+1, tr,prices,dp);
+                    profit = max(take,nottake);
+
+                }
+                else
+                {
+                    int take = +prices[index] + solve(index+1, tr+1,prices,dp);
+                    int nottake = 0 + solve(index+1, tr,prices,dp);
+                    profit = max(take,nottake);
+                }
+                dp[index][tr] = profit;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
