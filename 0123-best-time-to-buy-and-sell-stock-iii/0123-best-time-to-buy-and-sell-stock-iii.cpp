1d9de2327b80b27a7d7f69ca2fa6e398dd582c76
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-21 15:27:11 +0530

    Time: 428 ms (32.24%), Space: 211.5 MB (36.55%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -39,10 +39,56 @@ int solve(int index, int buy, int cap, vector<int>& prices,vector<vector<vector<
 
         n = prices.size();
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
 
-        return solve(0,1,2,prices,dp);
+        // return solve(0,1,2,prices,dp);
         // {index, buy, cap}
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+
+        for(int index = 0; index <= n; index++)
+        {
+            for(int buy = 0; buy<2; buy++)
+            {
+                dp[index][buy][0] = 0;
+            }
+        }
+
+        for(int buy = 0; buy<2; buy++)
+        {
+            for(int cap = 0; cap<3; cap++)
+            {
+                dp[n][buy][cap] = 0;
+            }
+        }
+
+        // we have set the base cases for these
+        for(int index = n-1;index>= 0; index--)
+        {
+            for(int buy = 0; buy<2; buy++)
+            {
+                for(int cap = 1; cap<3; cap++)
+                {
+                    int profit = INT_MIN;
+                    if(buy == 1)
+                    {
+                        int take = -prices[index] + solve(index+1, 0,cap,prices,dp);
+                        int nottake = 0 + solve(index+1,1, cap, prices,dp);
+                        profit = max(take,nottake);
+                    }
+
+                    else // sell krne ka option h
+                    {
+                        int take = +prices[index] + solve(index+1, 1,cap-1,prices,dp);
+                        int nottake = 0 + solve(index+1,0, cap, prices,dp);
+                        profit = max(take,nottake);
+                    }
+
+                    dp[index][buy][cap] = profit;   
+                }
+            }
+        }
+       return dp[0][1][2];
+
         
     }
 };
