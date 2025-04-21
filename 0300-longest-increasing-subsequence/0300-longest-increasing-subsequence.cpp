c67fd96acab64c6be061c3ca2414ac35692bcfa9
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-21 19:10:43 +0530

    Time: 557 ms (15.39%), Space: 296.5 MB (11.39%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+int n;
+
+int solve(int index, int previndex, vector<int>& nums, vector<vector<int>>& dp)
+{
+    if(index >= n)
+    {
+        return 0;
+    }
+    if(dp[index][previndex + 1] != -1)
+    {
+        return dp[index][previndex+1];
+    }
+
+    int len = INT_MIN;
+    int not_take = 0 + solve(index+1, previndex,nums,dp);
+    int take = 0;
+    if(previndex == -1 || nums[index] > nums[previndex])
+    {
+        take = 1 + solve(index+1,index, nums,dp);
+    }
+  
+    return dp[index][previndex+1] = len = max(take, not_take);
+}
+    int lengthOfLIS(vector<int>& nums) {
+
+        n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // we have made our previndex shifted by 1 place ahead, to actually cater the -1
+        
+        return solve(0,-1,nums, dp);
+        // we have {index, previndex}
+        // in the beginning we have -1 as our previndex
+    }
+};
