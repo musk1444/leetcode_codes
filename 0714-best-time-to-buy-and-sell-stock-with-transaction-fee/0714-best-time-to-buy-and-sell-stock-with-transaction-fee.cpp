98d9116c1360eea5fd2f77ad7f80a40dd263f73c
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-21 18:44:02 +0530

    Time: 16 ms (63.3%), Space: 58.8 MB (97.16%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -37,33 +37,36 @@ public:
         // vector<vector<int>> dp(n+1, vector<int>(2, -1));
         // return solve(0,1,prices,fee,dp);
 
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        vector<int> after(2, 0);
+        vector<int> curr(2, 0);
+
 
         for (int buy = 0; buy < 2; buy++) {
-            dp[n][buy] = 0;
+            curr[buy] = 0;
         }
 
         for (int index = n - 1; index >= 0; index--) {
             for (int buy = 0; buy < 2; buy++) {
                 int profit = INT_MIN;
                 if (buy == 1) {
-                    int take = -prices[index] - fee + dp[index + 1][0];
-                    int nottake = 0 + dp[index + 1][1];
+                    int take = -prices[index] - fee + after[0];
+                    int nottake = 0 + after[1];
 
                     profit = max(take, nottake);
                 } else // you can sell, now you can choose from either seeling
                        // or not selling
                 {
-                    int take = +prices[index] + dp[index + 1][1];
-                    int nottake = 0 + dp[index + 1][0];
+                    int take = +prices[index] + after[1];
+                    int nottake = 0 + after[0];
 
                     profit = max(take, nottake);
                 }
 
-                dp[index][buy] = profit;
+                curr[buy] = profit;
             }
+            after = curr;
         }
 
-        return dp[0][1];
+        return after[1];
     }
 };
