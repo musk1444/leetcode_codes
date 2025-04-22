d84af23deb5f330d792290329895ba707346b8b1
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-22 16:35:54 +0530

    Time: 0 ms (100%), Space: 14 MB (94.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,46 +3,24 @@ public:
     int lengthOfLIS(vector<int>& nums) {
 
         int n = nums.size();
-        vector<int> dp(n,1);
-        vector<int> hash(n);
-
-        int maxi = 1;
-        int lastindex = 0;
-
-        for(int index = 0; index < n;index++)
-        {
-            hash[index] = index;
-            for(int prev = 0; prev<=index - 1; prev++)
-            {
-                if(nums[prev] < nums[index] &&  (1 + dp[prev] > dp[index]))
-                {
-                    // agar humara prev chota h abhi wale element se, to humara subsequence ban skta h
-                    dp[index] = 1 + dp[prev];
-                    hash[index] = prev;
-                }
-            }
-
-            if(dp[index] > maxi)
-            {
-                maxi = dp[index];
-                lastindex = index;
-            }
-        }
         vector<int> temp;
-        temp.push_back(nums[lastindex]);
-        while(hash[lastindex] != lastindex)
+        temp.push_back(nums[0]);
+
+        for(int i = 1;i<n; i++)
         {
-            lastindex = hash[lastindex];
-            temp.push_back(nums[lastindex]);
+            if(temp.back() < nums[i])
+            {
+                temp.push_back(nums[i]);
+            }
+            else
+            {
+                // we find where should we insert our element
+                int index = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[index] = nums[i];
+            }
         }
 
-        reverse(begin(temp), end(temp));
-        for(auto it: temp)
-        {
-            cout << it << " ";
-        }
-
-        return maxi;
+        return temp.size();
         
     }
 };
