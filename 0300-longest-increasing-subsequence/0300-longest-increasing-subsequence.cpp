cb7aab2ecee82d927767741bb2694aa7e73e0570
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-22 15:16:22 +0530

    Time: 72 ms (49.77%), Space: 14.4 MB (60.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,58 +1,23 @@
 class Solution {
 public:
-    int n;
-
-    int solve(int index, int previndex, vector<int>& nums,
-              vector<vector<int>>& dp) {
-        if (index >= n) {
-            return 0;
-        }
-        if (dp[index][previndex + 1] != -1) {
-            return dp[index][previndex + 1];
-        }
-
-        int len = INT_MIN;
-        int not_take = 0 + solve(index + 1, previndex, nums, dp);
-        int take = 0;
-        if (previndex == -1 || nums[index] > nums[previndex]) {
-            take = 1 + solve(index + 1, index, nums, dp);
-        }
-
-        return dp[index][previndex + 1] = len = max(take, not_take);
-    }
     int lengthOfLIS(vector<int>& nums) {
 
-        n = nums.size();
+        int n = nums.size();
+        vector<int> dp(n,1);
 
-        // vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        // we have made our previndex shifted by 1 place ahead, to actually
-        // cater the -1
-
-        // return solve(0,-1,nums, dp);
-        // we have {index, previndex}
-        // in the beginning we have -1 as our previndex
-
-        // vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
-
-
-        vector<int> after(n + 1, 0);
-        vector<int> curr(n + 1, 0);
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int previndex = index - 1; previndex >= -1; previndex--) {
-
-                int len = INT_MIN;
-                int not_take = 0 + after[previndex + 1];
-                int take = 0;
-                if (previndex == -1 || nums[index] > nums[previndex]) {
-                    take = 1 + after[index + 1];
+        for(int index = 0; index < n;index++)
+        {
+            for(int prev = 0; prev<=index - 1; prev++)
+            {
+                if(nums[prev] < nums[index])
+                {
+                    // agar humara prev chota h abhi wale element se, to humara subsequence ban skta h
+                    dp[index] = max(dp[index], 1 + dp[prev]);
                 }
-
-                curr[previndex + 1] = len = max(take, not_take);
             }
-            after = curr;
         }
 
-        return after[-1 +1];
+        return *max_element(begin(dp), end(dp));
+        
     }
 };
