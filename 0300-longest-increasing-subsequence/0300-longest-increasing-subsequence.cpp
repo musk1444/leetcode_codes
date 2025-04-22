2ba981fa24ff71f0615b1c8ce4d358e17fbae3f2
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-22 13:33:34 +0530

    Time: 267 ms (32.1%), Space: 296.4 MB (18.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
+    int n;
 
-int n;
+    int solve(int index, int previndex, vector<int>& nums,
+              vector<vector<int>>& dp) {
+        if (index >= n) {
+            return 0;
+        }
+        if (dp[index][previndex + 1] != -1) {
+            return dp[index][previndex + 1];
+        }
 
-int solve(int index, int previndex, vector<int>& nums, vector<vector<int>>& dp)
-{
-    if(index >= n)
-    {
-        return 0;
-    }
-    if(dp[index][previndex + 1] != -1)
-    {
-        return dp[index][previndex+1];
-    }
+        int len = INT_MIN;
+        int not_take = 0 + solve(index + 1, previndex, nums, dp);
+        int take = 0;
+        if (previndex == -1 || nums[index] > nums[previndex]) {
+            take = 1 + solve(index + 1, index, nums, dp);
+        }
 
-    int len = INT_MIN;
-    int not_take = 0 + solve(index+1, previndex,nums,dp);
-    int take = 0;
-    if(previndex == -1 || nums[index] > nums[previndex])
-    {
-        take = 1 + solve(index+1,index, nums,dp);
+        return dp[index][previndex + 1] = len = max(take, not_take);
     }
-  
-    return dp[index][previndex+1] = len = max(take, not_take);
-}
     int lengthOfLIS(vector<int>& nums) {
 
         n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        // we have made our previndex shifted by 1 place ahead, to actually cater the -1
-        
-        return solve(0,-1,nums, dp);
+        // vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // we have made our previndex shifted by 1 place ahead, to actually
+        // cater the -1
+
+        // return solve(0,-1,nums, dp);
         // we have {index, previndex}
         // in the beginning we have -1 as our previndex
-        
+
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int previndex = index - 1; previndex >= -1; previndex--) {
+
+                int len = INT_MIN;
+                int not_take = 0 + dp[index + 1][previndex + 1];
+                int take = 0;
+                if (previndex == -1 || nums[index] > nums[previndex]) {
+                    take = 1 + dp[index + 1][index + 1];
+                }
+
+                dp[index][previndex + 1] = len = max(take, not_take);
+            }
+        }
+
+        return dp[0][-1 +1];
     }
 };
