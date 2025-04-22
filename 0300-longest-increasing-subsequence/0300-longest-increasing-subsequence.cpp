0f7bbadc2295c34dc95a06612845771f9ecb9d55
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-22 15:45:05 +0530

    Time: 89 ms (41.11%), Space: 14.4 MB (45.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,20 +4,42 @@ public:
 
         int n = nums.size();
         vector<int> dp(n,1);
+        vector<int> hash(n);
 
         int maxi = 1;
+        int lastindex = 0;
 
         for(int index = 0; index < n;index++)
         {
+            hash[index] = index;
             for(int prev = 0; prev<=index - 1; prev++)
             {
-                if(nums[prev] < nums[index])
+                if(nums[prev] < nums[index] &&  (1 + dp[prev] > dp[index]))
                 {
                     // agar humara prev chota h abhi wale element se, to humara subsequence ban skta h
-                    dp[index] = max(dp[index], 1 + dp[prev]);
+                    dp[index] = 1 + dp[prev];
+                    hash[index] = prev;
                 }
             }
-            maxi = max(maxi, dp[index]);
+
+            if(dp[index] > maxi)
+            {
+                maxi = dp[index];
+                lastindex = index;
+            }
+        }
+        vector<int> temp;
+        temp.push_back(nums[lastindex]);
+        while(hash[lastindex] != lastindex)
+        {
+            lastindex = hash[lastindex];
+            temp.push_back(nums[lastindex]);
+        }
+
+        reverse(begin(temp), end(temp));
+        for(auto it: temp)
+        {
+            cout << it << " ";
         }
 
         return maxi;
