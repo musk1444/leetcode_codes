b86b1805d7c01a65995ffd81470ca69df8a374f9
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-27 14:35:53 +0530

    Time: 56 ms (63.62%), Space: 126.9 MB (10.48%) - LeetHub

--- 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-        #define int long long int 
-        int ans = 0;
+    long long countInterestingSubarrays(vector<int>& nums, int m, int k) {
+        
         int n = nums.size();
-        for(int i = 0; i < n;i++)
+        for(int i = 0; i<n; i++)
         {
-            if (nums[i] % modulo == k)
+            if(nums[i] %m == k)
             {
                 nums[i] = 1;
+                // to make sure that elements that are divisible get marked as 1
             }
             else
             {
                 nums[i] = 0;
             }
         }
-        unordered_map <int, int> mp;
-        int total = 0;
-        for(int i = 0; i < n;i++)
+
+        long long sum = 0;
+        long long result = 0;
+        unordered_map<int, long long> mpp;
+        mpp[0] = 1;
+        
+        for(int i = 0; i<n; i++)
         {
-            total += nums[i];
-            int mod_val = total % modulo;
-            if (mod_val == k) ans++;
-            int find = mod_val - k;
-            if (find < 0)
-            {
-                find += modulo;
-            }
-            ans += mp[find];
-            mp[mod_val]++;
+            sum += nums[i];
+            int r1 = sum%m;
+            // we get the forst remainder
+
+            int r2 = (r1 - k + m)%m;
+            result += mpp[r2];
+
+            mpp[r1]++;
+
         }
-        return ans;
-        #undef int 
+
+        return result;
     }
 };
