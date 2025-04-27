0c3fa752cc1458482286a600ae9120989d4135f1
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-04-27 13:19:11 +0530

    Time: 19 ms (18.69%), Space: 35.6 MB (48.69%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        unordered_map<int,int> mpp;
+        
+        // mpp[0] = 1; as we have already seen the remainder 0
+        int sum = 0;
+        mpp[0] = 1;
+        int result = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            sum += nums[i];
+
+            int remainder = sum%k;
+            if(remainder < 0)
+            {
+                remainder += k;
+            }
+            // now we will check if we have previosly seen this remainder
+            if(mpp.find(remainder) != mpp.end())
+            {
+                result += mpp[remainder];
+            }
+            mpp[remainder]++;
+        }
+        
+        return result;
+    }
+};
