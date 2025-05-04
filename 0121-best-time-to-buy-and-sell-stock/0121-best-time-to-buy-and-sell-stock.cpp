ddc7bf7bceef4ace5761cb6d0762060da031d582
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-04 18:15:36 +0530

    Time: 4 ms (19.38%), Space: 97.4 MB (61.6%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
+        int n = prices.size();
         int mini = prices[0];
         int profit = 0;
-        int n = prices.size();
 
         for(int i = 1; i<n; i++)
         {
             int cost = prices[i] - mini;
             profit = max(profit, cost);
-            // now we need to updare the mini value
             mini = min(mini, prices[i]);
         }
 
