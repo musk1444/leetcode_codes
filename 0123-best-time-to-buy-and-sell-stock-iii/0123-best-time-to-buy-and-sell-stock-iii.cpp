69041200835754c0fd0890c1621b22c1840a127d
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-04 19:20:09 +0530

    Time: 424 ms (33.2%), Space: 211.4 MB (46.72%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,49 +1,76 @@
 class Solution {
 public:
-int n;
+    int n;
 
-int solve(vector<int>& prices, int index, int buy, int cap,vector<vector<vector<int>>>& dp)
-{
-    if(index >= n) return 0;
-    if(cap == 0) return 0;
-    // means we have exhausted our limit
+    int solve(vector<int>& prices, int index, int buy, int cap,
+              vector<vector<vector<int>>>& dp) {
+        if (index >= n)
+            return 0;
+        if (cap == 0)
+            return 0;
+        // means we have exhausted our limit
 
-    if(dp[index][buy][cap] != -1)
-    {
-        return dp[index][buy][cap];
+        if (dp[index][buy][cap] != -1) {
+            return dp[index][buy][cap];
+        }
+
+        int profit = INT_MIN;
+
+        if (buy == 1) {
+            int take = -prices[index] + solve(prices, index + 1, 0, cap, dp);
+            int nottake = solve(prices, index + 1, 1, cap, dp);
+
+            profit = max(take, nottake);
+        } else // you can sell
+        {
+            int take =
+                +prices[index] + solve(prices, index + 1, 1, cap - 1, dp);
+            int nottake = solve(prices, index + 1, 0, cap, dp);
+
+            profit = max(take, nottake);
+        }
+
+        return dp[index][buy][cap] = profit;
     }
-
-    int profit = INT_MIN;
-
-    if(buy == 1)
-    {
-        int take = -prices[index] + solve(prices,index+1,0,cap,dp);
-        int nottake = solve(prices,index+1,1,cap,dp);
-
-        profit = max(take,nottake);
-    }
-    else // you can sell
-    {
-        int take = +prices[index] + solve(prices,index+1,1,cap-1,dp);
-        int nottake = solve(prices,index+1,0,cap,dp);
-
-        profit = max(take,nottake);
-    }
-
-    return dp[index][buy][cap] = profit;
-}
 
     int maxProfit(vector<int>& prices) {
 
         n = prices.size();
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2,
+        // vector<int>(3,-1)));
 
-        return solve(prices,0,1,2,dp);
+        // return solve(prices,0,1,2,dp);
         // index, buy, cap
 
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
 
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    int profit = INT_MIN;
 
-        
+                    if (buy == 1) {
+                        int take = -prices[index] +
+                                   solve(prices, index + 1, 0, cap, dp);
+                        int nottake = solve(prices, index + 1, 1, cap, dp);
+
+                        profit = max(take, nottake);
+                    } else // you can sell
+                    {
+                        int take = +prices[index] +
+                                   solve(prices, index + 1, 1, cap - 1, dp);
+                        int nottake = solve(prices, index + 1, 0, cap, dp);
+
+                        profit = max(take, nottake);
+                    }
+
+                    dp[index][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
