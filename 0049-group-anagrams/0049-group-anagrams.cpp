405ba46dff5502e1d19f9b698b5f15e8fb0e56f7
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-05 00:13:37 +0530

    Time: 18 ms (52.71%), Space: 27.2 MB (22.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        int n = strs.size();
 
+string generate(string& word)
+{
+    int hash[26] = {0};
+
+    for(auto &ch:word)
+    {
+        hash[ch-'a']++;
+        // we keep addign the character frequency
+    }
+
+    string new_word = "";
+    for(int i = 0; i<26; i++)
+    {
+        int freq = hash[i]; // take out the frequency of that character
+        if(freq > 0)
+        {
+           new_word += string(freq, i-'a');
+        }
+    }
+
+    return new_word;
+
+}
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n = strs.size();
         unordered_map<string, vector<string>> mpp;
+        
         vector<vector<string>> result;
 
         for(int i = 0; i<n; i++)
         {
-            string temp = strs[i];
-            // sort krdp temp ko
-            sort(begin(temp), end(temp));
+            string word = strs[i];
+            string new_word = generate(word);
 
-            mpp[temp].push_back(strs[i]);
+            mpp[new_word].push_back(word);
         }
+
         for(auto it:mpp)
         {
             result.push_back(it.second);
         }
 
         return result;
+        
     }
 };
