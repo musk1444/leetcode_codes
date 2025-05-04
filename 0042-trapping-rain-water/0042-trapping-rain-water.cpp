769105553657d7c4d32acbbcff25f36f225bb0b3
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-04 23:37:12 +0530

    Time: 0 ms (100%), Space: 27.3 MB (30.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,26 +4,26 @@ public:
 
         int n = height.size();
 
-        vector<int> leftmax(n);
+        vector<int> leftmax(n,INT_MIN);
+        vector<int> rightmax(n,INT_MIN);
+
         leftmax[0] = height[0];
+        rightmax[n-1] = height[n-1];
+
         for(int i = 1; i<n; i++)
         {
-            leftmax[i] = max(leftmax[i-1], height[i]);
+            leftmax[i] = max(leftmax[i-1],height[i]);
         }
-        vector<int> rightmax(n);
-        rightmax[n-1] = height[n-1];
         for(int i = n-2; i>=0; i--)
         {
-            rightmax[i] = max(rightmax[i+1], height[i]);
+            rightmax[i] = max(rightmax[i+1],height[i]);
         }
-
         int total = 0;
-        
-        for(int i = 1; i<n; i++)
+        for(int i = 0; i<n; i++)
         {
-            total += min(leftmax[i], rightmax[i]) - height[i];
+            total += min(leftmax[i],rightmax[i]) - height[i];
         }
+        return total;        
 
-        return total;
     }
 };
