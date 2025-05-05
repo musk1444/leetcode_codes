4483fa182f9d7e263385fa49ce59ae2c6c3bba67
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-05 11:32:22 +0530

    Time: 0 ms (100%), Space: 8 MB (62.69%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -3,54 +3,55 @@ public:
     string fractionAddition(string exp) {
 
         int n = exp.length();
-        int nume = 0;
-        int deno = 1;
-
         int i = 0;
-        while(i < n)
-        {
-            int currnume = 0;
-            int currdeno = 0;
+        bool isneg = false;
 
-            bool isneg = (exp[i] == '-');
-            if(exp[i] == '+' || exp[i] == '-')
+        int num = 0;
+        int den = 1;
+
+        while(i<n)
+        {
+            int currnum = 0;
+            int currden = 0;
+            
+            isneg = (exp[i] == '-');
+
+            while(exp[i] == '+' || exp[i] == '-')
             {
-                // jab tak ye expressions h to aag ebdho
                 i++;
+            }
+            // now we will find out the numerator
+            while(i<n && (isdigit(exp[i])))
+            {
+                int val = exp[i] - '0';
+                currnum = currnum*10 + val;
+                i++;
+            }
+            i++; // this is to skip the '/' divisor operator
+            if(isneg)
+            {
+                currnum = currnum*-1;
             }
 
             while(i<n && (isdigit(exp[i])))
             {
                 int val = exp[i] - '0';
-                currnume = currnume*10 + val;
+                currden = currden*10 + val;
                 i++;
             }
-            i++; // this is done to skip the divisor character '/'
 
-            if(isneg == true)
-            {
-                currnume *= -1;
-            }
+            num = den*currnum + num*currden;
+            den = den*currden;
 
-            while(i < n && (isdigit(exp[i])))
-            {
-                // build the currdenominator
-                int val = exp[i] - '0';
-                currdeno = currdeno*10 + val;
-                i++;
-            }
-            // ab result nikaal lo
-            nume = nume*currdeno + deno*currnume;
-            deno = deno*currdeno;
+            int gcd = abs(__gcd(num,den));
+
+            num = num/gcd;
+            den = den/gcd;
         }
 
-        int gcd = abs(__gcd(nume,deno));
-        // jais eki -3,6 ka gcd 3 hona chhaiye but isme dikat aajati h corner case me
+        return to_string(num) + '/' + to_string(den);
 
-        nume = nume/gcd;
-        deno = deno/gcd;
-
-        return to_string(nume) + '/' + to_string(deno);
+                                              
         
     }
 };
