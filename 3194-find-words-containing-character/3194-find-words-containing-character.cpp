bf06221d280b16245f4c30a68c7a074f8604ebcf
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-25 23:26:26 +0530

    Time: 4 ms (15.83%), Space: 33.7 MB (16.86%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector <int> res;
-        for (int i = 0 ; i < words.size() ; i++) 
-            for (int j = 0 ; j < words[i].size() ; j++) 
-                if (words[i][j] == x) {
-                    res.push_back(i);
+
+        vector<int> ans;
+        for(int i = 0; i<words.size(); i++)
+        {
+            string word = words[i];
+            for(int j = 0;j<word.length(); j++)
+            {
+                if(word[j] == x)
+                {
+                    ans.push_back(i);
                     break;
                 }
-        return res;
+            }
+        }
+
+        return ans;
+        
     }
 };
