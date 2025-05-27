14739e65dbdc5c440e4270dc973283a302d1629e
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-05-27 23:08:35 +0530

    Time: 1995 ms (5.01%), Space: 26.1 MB (90.68%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,12 +1,33 @@
 class Solution {
-    public:
-        int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-            map<pair<int, int>, int> mpp;
-            int count = 0;
-            for(auto& d : dominoes){
-                if(d[0] > d[1]) swap(d[0], d[1]);
-                count += mpp[make_pair(d[0], d[1])]++;
+public:
+
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+
+        for(auto &d: dominoes)
+        {
+            if(d[0] > d[1])
+            {
+                swap(d[1],d[0]);
             }
-            return count;
         }
-    };
+
+        int n = dominoes.size();
+        // pehle sort krdiya
+        int count = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(dominoes[i][0] == dominoes[j][0] && dominoes[i][1] == dominoes[j][1])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+        
+    }
+};
