28ea0078ab0a087deb002635a682f5eb317e0aad
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-07-19 19:30:14 +0530

    Time: 134 ms (76.46%), Space: 154.6 MB (59.75%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
-
+        
         int N = nums.size();
         int n = N/3;
 
         vector<long long> leftminsum(N,0);
-        // we will need 2 vectors to store the leftminsum until i
-        // and the rightmax sum as well
         vector<long long> rightmaxsum(N,0);
 
         priority_queue<int> maxheap;
         long long leftsum = 0;
+
         for(int i = 0; i<2*n; i++)
         {
-            // we will keep adding the number
-            maxheap.push(nums[i]);
             leftsum += nums[i];
+            maxheap.push(nums[i]);
 
             if(maxheap.size() > n)
             {
@@ -27,13 +25,14 @@ public:
             leftminsum[i] = leftsum;
         }
 
+        // we will do the similar for rightmaxsum
         priority_queue<int, vector<int>, greater<int>> minheap;
         long long rightsum = 0;
 
         for(int i = N-1; i>=n; i--)
         {
-            minheap.push(nums[i]);
             rightsum += nums[i];
+            minheap.push(nums[i]);
 
             if(minheap.size() > n)
             {
@@ -44,14 +43,12 @@ public:
             rightmaxsum[i] = rightsum;
         }
 
-        //we will now find the result
+        // we will now calculate the result
         long long result = LLONG_MAX;
-        for(int i = n-1; i<=2*n-1; i++)
+        for(int i = n-1; i<=2*n-1;i++)
         {
             result = min(result, leftminsum[i] - rightmaxsum[i+1]);
         }
-
         return result;
     }
-        
 };
