211ca8e99bcf476d7b0ae6f419f11cd47b60d8af
musk1444 <132774661+musk1444@users.noreply.github.com>
2025-07-20 11:13:39 +0530

    Time: 123 ms (32.91%), Space: 83.3 MB (25.27%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        // Sort the folders lexicographically so parent folders come before their subfolders
-        sort(folder.begin(), folder.end());
 
-        // Initialize result vector with the first folder
-        vector<string> ans;
-        ans.push_back(folder[0]);
+        int n = folder.size();
+        unordered_set<string> st(begin(folder), end(folder));
 
-        // Iterate through remaining folders starting from index 1
-        for(int i = 1; i < folder.size(); i++) {
-            // Get the last added folder path and add a trailing slash
-            // This helps in comparing if current folder is a subfolder
-            string lastFolder = ans.back();
-            lastFolder.push_back('/');
+        vector<string> result;
+        for(auto currfolder:folder)
+        {
+            bool issubfolder = false;
+            string tempfolder = currfolder;
 
-            // Compare current folder with last added folder
-            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
-            // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
-            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
-                // If not a subfolder, add to result
-                ans.push_back(folder[i]);
+            while(!currfolder.empty())
+            {
+                auto position = currfolder.find_last_of('/');
+                currfolder = currfolder.substr(0,position);
+
+                if(st.find(currfolder) != st.end())
+                {
+                    // it means currfolder is a subfolder
+                    issubfolder = true;
+                    break;
+                }
+            }
+            if(!issubfolder)
+            {
+                result.push_back(tempfolder);
             }
         }
 
-        return ans;
+        return result;
+        
     }
 };
